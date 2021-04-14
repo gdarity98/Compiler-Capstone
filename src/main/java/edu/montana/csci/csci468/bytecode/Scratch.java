0fa53611466d4202d4b7b0f602648078e9b5ab93
gdarity98 <gdarity98@gmail.com>
2021-04-14 13:42:13 -0600

    Working on ByteCode
    Working on byte code, added listliteralexpression and a place for scratch work to generate bytecode

---
+++ src/main/java/edu/montana/csci/csci468/bytecode/Scratch.java
@@ -0,0 +1,14 @@
+package edu.montana.csci.csci468.bytecode;
+
+public class Scratch {
+
+    public void main(){
+        var x = 10;
+        var y = 20;
+        var z = x + y;
+    }
+
+    public int intFunc(int i1, int i2) {
+        return i1 + i2;
+    }
+}
