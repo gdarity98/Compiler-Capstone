e8d9f7c195e60c883cc20c6dbd3bc982ed7467bd
gdarity98 <gdarity98@gmail.com>
2021-04-27 02:54:58 -0600

    Update PartnersTest.java
    added comments to partner tests

--- src/test/java/PartnersTest.java
+++ src/test/java/PartnersTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PartnersTest extends CatscriptTestBase {
     @Test
+    // Test 1 tested the execution of variable statements, for statements, and the if statement.
     void testOne() {
         assertEquals("1\n2\n3\n", executeProgram("var y = true\n" +
                 "if(y) {" +
@@ -14,11 +15,13 @@ public class PartnersTest extends CatscriptTestBase {
     }
 
     @Test
+    // Test 2 tested if a list literal expression, [1+2, 2-1, 3/1], would compile to bytecode.
     void testTwo() {
         assertEquals("[3, 1, 3]\n", compile("[1+2, 2-1, 3/1]"));
     }
 
     @Test
+    // Test 3 tested the execution of a function and comparison expressions within an if statement.
     void testThree() {
         assertEquals("8\n9\n12\n", executeProgram("function foo(x : int) {" +
                 "if(x > 5){" +
